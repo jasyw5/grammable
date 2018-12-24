4df4b3c739159a98ed464276f55c450e979d2a49
John Stahl <john.stahl24@gmail.com>
2018-12-24 15:59:08 +0000

    added the comments tests

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -1,5 +1,5 @@
 class GramsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]  
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]  
 
 
   def new
@@ -9,11 +9,13 @@ class GramsController < ApplicationController
   def edit
     @gram = Gram.find_by_id(params[:id])
     return render_not_found if @gram.blank?
+    return render_not_found(:forbidden) if @gram_user != current_user
   end
 
   def update
     @gram = Gram.find_by_id(params[:id])
     return render_not_found if @gram.blank?
+    return render_not_found(:forbidden) if @gram.user != current_user
 
     @gram.update_attributes(gram_params)
     if @gram.valid?
@@ -29,6 +31,7 @@ class GramsController < ApplicationController
   end 
 
   def index 
+    @grams = Gram.all
   end
 
   def create
@@ -40,15 +43,22 @@ class GramsController < ApplicationController
     end
   end
 
+  def destroy
+    @gram = Gram.find_by_id(params[:id])
+    return render_not_found if @gram.blank?
+    return render_not_found(:forbidden) if @gram.user != current_user
+    @gram.destroy
+    redirect_to root_path
+  end
 
   private
 
   def gram_params
-    params.require(:gram).permit(:message)
+    params.require(:gram).permit(:message, :picture)
   end
 
-  def render_not_found
-    render plain: 'Not Found', status: :not_found
+  def render_not_found(status=:not_found)
+    render plain: "#{status.to_s.titleize}", status: status
   end
 
 end
